c0a71a36e156321ca3dfcb85960defea9bd9dd0a
pToum21 <pey2moo1@gmail.com>
2023-12-20 11:57:53 -0800

    add get single thought and get all thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,25 @@
 const { Thought, User } = require('../models');
 
 module.exports = {
-    
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(err)
+        }
+    },
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
+            if (!thought) {
+                res.status(404).json({ message: 'no thought with that id found' })
+            }
+            res.json(thought)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(err)
+        }
+    }
 }
