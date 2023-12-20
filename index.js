c2914f4f96f2edc06243240dbd243078f76ae249
toum21 <pey2moo1@gmail.com>
2023-12-19 20:37:13 -0800

    updated package.json to have nodemon to always run server while work to catch errors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
