3e960e6b650ca392b9ed3924232783716b817780
pToum21 <pey2moo1@gmail.com>
2023-12-20 12:43:46 -0800

    updated addReaction and delete reaction completing the thoughtController for the time being

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -68,9 +68,37 @@ module.exports = {
         }
     },
     async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $addToSet: { reactions: req.body } },
+                { runValidators: true, new: true }
+            )
 
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+            res.json(thought)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(err)
+        }
     },
     async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: { reactionId: req.params.reactionId } } },
+                { runValidators: true, new: true }
+            )
 
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+            res.json(thought)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(err)
+        }
     },
 };
