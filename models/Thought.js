d116e97001fd6c3acc87687437882f7167d0611d
toum21 <pey2moo1@gmail.com>
2023-12-19 22:58:14 -0800

    added a nested schema for reactions inside of thoughts

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function (value) {
+                return value.toLocaleString();
+            }
+        }
+    }
+);
+
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -19,6 +45,25 @@ const thoughtSchema = new Schema(
             type: String,
             required: true
         },
-        
+        reactions: [reactionSchema]
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length
+    })
+
+const Thought = model('thought', thoughtSchema)
+
+module.exports = Thought;
+
