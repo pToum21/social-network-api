f23376743659955c38ddd091aa809e0ab8600245
pToum21 <pey2moo1@gmail.com>
2023-12-20 11:11:01 -0800

    working on thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,6 +3,17 @@ const { User, Thought } = require('../../models');
 const 
 {
     getThoughts,
+    addThoughts,
+    getSingleThought,
+    updateThought,
+    deleteThought
+    
+
     // add other controllers in here
 } = require('../../controllers/thoughtController');
 
+router.route('/').get(getThoughts).post(addThoughts);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').get()
