2f8499d72c4e686d9b2f1de124ac20d18420cf38
toum21 <pey2moo1@gmail.com>
2023-12-19 18:32:07 -0800

    created data in utils for user

---
+++ utils/data.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        username: 'Brick',
+        email:'bigjucier@gmail.com'
+    },
+    {
+        username: 'Loaf',
+        email:'loaf@gmail.com'
+    },
+    {
+        username: 'Pizz',
+        email:'pizz@gmail.com'
+    },
+    {
+        username: 'Victoria',
+        email:'wife@gmail.com'
+    },
+];
+
+module.exports = users
