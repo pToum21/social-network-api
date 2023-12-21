f2be0a7f2bb2ae93aaed5a28f3bfc01e4355b06e
pToum21 <pey2moo1@gmail.com>
2023-12-21 15:21:47 -0500

    route all controllers must test them all now and figure out to seed the databse

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,7 +89,22 @@ module.exports = {
         }
     },
     async deleteFriend(req, res) {
+        try {
+            const friend = await User.findByIdAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            )
 
+            if (!friend) {
+                return res.status(404).json({ message: 'No user with that ID' })
+            }
+            res.json(friend)
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(err)
+        }
     }
 
 
