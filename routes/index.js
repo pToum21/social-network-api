9bd974976e9291c7d93422208dcc17970e1ef729
pToum21 <pey2moo1@gmail.com>
2023-12-23 20:42:13 -0500

    added comments

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+// append api in fron of all routes
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
--- utils/data.js
+++ utils/data.js
@@ -1,3 +1,4 @@
+// seeded users
 const users = [
     {
         username: 'Brick',
