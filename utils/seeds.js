0e8a9e0bfcc82a733b8183fd6f0876d0df3b5933
pToum21 <pey2moo1@gmail.com>
2023-12-23 20:43:22 -0500

    added comments to utils folder

--- utils/seeds.js
+++ utils/seeds.js
@@ -2,11 +2,12 @@ const connection = require('../config/connection')
 const { User } = require('../models');
 const users = require('./data')
 
+// et up a one-time event listener for the 'open' event on the database connection
 connection.once('open', async () => {
     console.log('connection complete!')
 
     let checkUser = await connection.db.listCollections({ name: 'users' }).toArray();
-
+    // If the 'users' collection exists, drop it
     if (checkUser) {
         await connection.dropCollection('users');
     }
